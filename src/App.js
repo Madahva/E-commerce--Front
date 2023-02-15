83fcb4ea65ca3df8556c554c577af144217b7615
becaale <becaale@hotmail.com>
2023-02-15 20:32:50 +0100

    fix(SRC):corregi rutas de importacion y nombre de carpeta productcatalog

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./assets/styles/App.css";
 import { Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar.jsx";
-import Home from "./components/Home.jsx";
-import ProductCatalog from "./components/ProductCatalog.jsx";
-import DetailsPage from "./components/DetailsPage.jsx";
-import Footer from "./components/Footer.jsx";
+import NavBar from "./components/NavBar/NavBar.jsx";
+import Home from "./components/Home/Home.jsx";
+import ProductCatalog from "./components/ProductCatalog/ProductCatalog.jsx";
+import DetailsPage from "./components/DetailsPage/DetailsPage.jsx";
+import Footer from "./components/Footer/Footer.jsx";
 
 const App = () => {
   return (
