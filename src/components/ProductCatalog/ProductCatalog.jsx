2b5c2668c0517852cd7a9bbfe9f96f805ef62ed2
oscar bejarano <osc4r.yt@gmail.com>
2023-02-17 15:31:10 -0500

    feat(orderBy):se organizo el componente productos y se agrego el orderBy

--- src/components/ProductCatalog/ProductCatalog.jsx
+++ src/components/ProductCatalog/ProductCatalog.jsx
@@ -1,18 +1,22 @@
 import * as React from "react";
 import { Link, useLocation } from "react-router-dom";
-import { styled } from "@mui/material/styles";
 import { makeStyles } from "@mui/styles";
 import Grid from "@mui/material/Grid";
-import Paper from "@mui/material/Paper";
+
 import Typography from "@mui/material/Typography";
-import ButtonBase from "@mui/material/ButtonBase";
+
 import { Box } from "@mui/system";
 import Stack from "@mui/material/Stack";
 import Container from "@mui/material/Container";
 import Breadcrumbs from "@mui/material/Breadcrumbs";
 import Divider from "@mui/material/Divider";
-import { products } from "../../assets/styles/Data.js";
 
+import Button from "@mui/material/Button";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import { useState } from "react";
+import Chip from "@mui/material/Chip";
+import Products from "../Products/Products";
 const useStyles = makeStyles((theme) => ({
   link: {
     color: "inherit",
@@ -36,22 +40,27 @@ const ProductCatalog = (props) => {
   const segments = location.pathname.split("/");
   const lastSegment = segments.pop();
   let categorieSeccion = decodeURIComponent(lastSegment);
+  const [buttonText, setButtonText] = useState("Most relevant");
 
   if (categorieSeccion === "Game Consoles") categorieSeccion = "GameConsoles";
   if (categorieSeccion === "Smart Watches") categorieSeccion = "SmartWatches";
 
-  const Img = styled("img")({
-    margin: "auto",
-    display: "block",
-    maxWidth: "100%",
-    maxHeight: "100%",
-  });
+  //en typescript se cambia
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = (event) => {
+    setButtonText(event.target.textContent);
+    setAnchorEl(null);
+  };
 
   return (
     <>
       <Container fixed>
         <Box sx={{ width: "100%" }}>
-          <Stack direction="column" spacing={12}>
+          <Stack direction="column" spacing={6}>
             <Breadcrumbs aria-label="breadcrumb">
               <Link className={classes.link} to="/">
                 Home
@@ -61,7 +70,6 @@ const ProductCatalog = (props) => {
               </Link>
               <Typography color="text.primary">{categorieSeccion}</Typography>
             </Breadcrumbs>
-
             <Grid
               container
               direction="row"
@@ -70,87 +78,84 @@ const ProductCatalog = (props) => {
               alignItems="flex-start"
             >
               <Grid item xs={3}>
-                <h2>Filters: </h2>
+                <Stack
+                  spacing={2}
+                  direction="column"
+                  justifyContent="center"
+                  alignItems="center"
+                >
+                  <h1>Filters: </h1>
+                  <Box sx={{ margin: 1.4 }}></Box>
+                  <h3>Prices</h3>
+                  <Chip label="Up to $50" />
+                  <Chip label="$50 to $100" />
+                  <Chip label="$100 to $500" />
+                  <Chip label="$500 to $1000" />
+                </Stack>
               </Grid>
               <Divider orientation="vertical" flexItem />
               <Grid item xs={8}>
                 <Grid
                   container
-                  rowSpacing={12}
+                  rowSpacing={2}
                   direction="column"
                   justifyContent="space-between"
                   alignItems="stretch"
                 >
-                  <h2>Show XX items of XX</h2>
-                  {products[categorieSeccion].map((el, index) => {
-                    return (
-                      <Link to={`/${categorieSeccion}/${index}`}>
-                        <Paper
-                          className={classes.product}
-                          sx={{
-                            p: 2,
-                            margin: "10px",
-                            maxWidth: 1280,
-                            flexGrow: 1,
-                            backgroundColor: (theme) =>
-                              theme.palette.mode === "dark"
-                                ? "#1A2027"
-                                : "#fff",
-                          }}
-                          key={index}
-                        >
-                          <Grid container spacing={2}>
-                            <Grid item>
-                              <ButtonBase sx={{ width: 128, height: 128 }}>
-                                <Img alt={el.name} src={el.image} />
-                              </ButtonBase>
-                            </Grid>
-                            <Grid item xs={12} sm container>
-                              <Grid
-                                item
-                                xs
-                                container
-                                direction="column"
-                                spacing={2}
-                              >
-                                <Grid item xs>
-                                  <Typography
-                                    gutterBottom
-                                    variant="subtitle1"
-                                    component="div"
-                                  >
-                                    {el.name}
-                                  </Typography>
-                                  <Typography variant="body2" gutterBottom>
-                                    {el.description}
-                                  </Typography>
-                                  <Typography
-                                    variant="body2"
-                                    color="text.secondary"
-                                  >
-                                    {el.brand}
-                                  </Typography>
-                                </Grid>
-                                <Grid item>
-                                  <Typography
-                                    sx={{ cursor: "pointer" }}
-                                    variant="body2"
-                                  >
-                                    {"Favorites <3"}
-                                  </Typography>
-                                </Grid>
-                              </Grid>
-                              <Grid item>
-                                <Typography variant="subtitle1" component="div">
-                                  {"$" + el.price}
-                                </Typography>
-                              </Grid>
-                            </Grid>
+                  <Grid item xs>
+                    <Grid
+                      container
+                      direction="row"
+                      justifyContent="space-between"
+                      alignItems="center"
+                      spacing={2}
+                    >
+                      <Grid item xs={4}>
+                        <h2>Show XX items of XX</h2>
+                      </Grid>
+                      <Grid item xs={4}>
+                        <Grid container spacing={2}>
+                          <Grid item xs={5}>
+                            <h3>Order by: </h3>
                           </Grid>
-                        </Paper>
-                      </Link>
-                    );
-                  })}
+                          <Grid item xs={7}>
+                            <Box sx={{ margin: 1.4 }}></Box>
+                            <Button
+                              id="basic-button"
+                              aria-controls={open ? "basic-menu" : undefined}
+                              aria-haspopup="true"
+                              aria-expanded={open ? "true" : undefined}
+                              onClick={handleClick}
+                            >
+                              {buttonText}
+                            </Button>
+                            <Menu
+                              id="basic-menu"
+                              anchorEl={anchorEl}
+                              open={open}
+                              onClose={handleClose}
+                              MenuListProps={{
+                                "aria-labelledby": "basic-button",
+                              }}
+                            >
+                              <MenuItem onClick={(e) => handleClose(e)}>
+                                Most relevant
+                              </MenuItem>
+                              <MenuItem onClick={(e) => handleClose(e)}>
+                                Lower price
+                              </MenuItem>
+                              <MenuItem onClick={(e) => handleClose(e)}>
+                                Higher price
+                              </MenuItem>
+                            </Menu>
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                  <Grid item xs>
+                    <Products categorieSeccion={categorieSeccion} />
+                  </Grid>
                 </Grid>
               </Grid>
             </Grid>
