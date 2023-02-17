a0f9826ee200989a122ae6817976695430050530
madahva <galarzaguillermo.ggm@gmail.com>
2023-02-17 13:23:53 -0300

    Feat (component/DetailPage): Cree la lógica de los detalles. Faltan stilos

--- src/components/DetailsPage/DetailsPage.jsx
+++ src/components/DetailsPage/DetailsPage.jsx
@@ -3,16 +3,22 @@ import { useParams } from "react-router-dom";
 import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles((theme) => ({
+  detailsPage: {
+    backgroundColor:"#ededed",
+  },
   detail: {
     alignItems: "center",
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
+    backgroundColor: "#fff",
     padding: "1rem",
+    width:"50vw",
+    margin:"0 auto"
   },
   productImage: {
     maxWidth: "350px",
-    width:"100%",
+    width: "100%",
     height: "auto",
   },
 }));
@@ -21,18 +27,20 @@ const DetailsPage = () => {
   const { category, id } = useParams();
   const product = products[category][id];
   return (
-    <div className={classes.detail}>
-      <h1>{product.name}</h1>
-      <img
-        src={product.image}
-        alt={product.name}
-        className={classes.productImage}
-      />
-      <div>
-        <p>{product.description}</p>
-        <p>{product.brand}</p>
-        <p>{product.price}</p>
-        <p>{product.size}</p>
+    <div className={classes.detailsPage}>
+      <div className={classes.detail}>
+        <h1>{product.name}</h1>
+        <img
+          src={product.image}
+          alt={product.name}
+          className={classes.productImage}
+        />
+        <div>
+          <p>{product.description}</p>
+          <p>{product.brand}</p>
+          <p>{product.price}</p>
+          <p>{product.size}</p>
+        </div>
       </div>
     </div>
   );
