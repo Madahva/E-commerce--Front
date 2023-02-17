49ace7e2105fa62ff996c1a364f98a2977236b5f
madahva <galarzaguillermo.ggm@gmail.com>
2023-02-17 04:31:02 -0300

    Feat (component/DetailPage): Cree la lógica de los detalles. Faltan stilos

--- src/components/DetailsPage/DetailsPage.jsx
+++ src/components/DetailsPage/DetailsPage.jsx
@@ -1,7 +1,41 @@
+import { products } from "../../assets/styles/Data";
+import { useParams } from "react-router-dom";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  detail: {
+    alignItems: "center",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    padding: "1rem",
+  },
+  productImage: {
+    maxWidth: "350px",
+    width:"100%",
+    height: "auto",
+  },
+}));
 const DetailsPage = () => {
+  const classes = useStyles();
+  const { category, id } = useParams();
+  const product = products[category][id];
   return (
-    <div>Details Page</div>
-  )
+    <div className={classes.detail}>
+      <h1>{product.name}</h1>
+      <img
+        src={product.image}
+        alt={product.name}
+        className={classes.productImage}
+      />
+      <div>
+        <p>{product.description}</p>
+        <p>{product.brand}</p>
+        <p>{product.price}</p>
+        <p>{product.size}</p>
+      </div>
+    </div>
+  );
 };
 
 export default DetailsPage;
--- src/components/ProductCatalog/ProductCatalog.jsx
+++ src/components/ProductCatalog/ProductCatalog.jsx
@@ -21,6 +21,13 @@ const useStyles = makeStyles((theme) => ({
       textDecoration: "underline",
     },
   },
+  product: {
+    cursor: "pointer",
+    "&:hover": {
+      backgroundColor: "#b0ecfd",
+      transition: "all ease-in .2s",
+    },
+  },
 }));
 
 const ProductCatalog = (props) => {
@@ -77,66 +84,71 @@ const ProductCatalog = (props) => {
                   <h2>Show XX items of XX</h2>
                   {products[categorieSeccion].map((el, index) => {
                     return (
-                      <Paper
-                        sx={{
-                          p: 2,
-                          margin: "10px",
-                          maxWidth: 1280,
-                          flexGrow: 1,
-                          backgroundColor: (theme) =>
-                            theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-                        }}
-                        key={index}
-                      >
-                        <Grid container spacing={2}>
-                          <Grid item>
-                            <ButtonBase sx={{ width: 128, height: 128 }}>
-                              <Img alt={el.name} src={el.image} />
-                            </ButtonBase>
-                          </Grid>
-                          <Grid item xs={12} sm container>
-                            <Grid
-                              item
-                              xs
-                              container
-                              direction="column"
-                              spacing={2}
-                            >
-                              <Grid item xs>
-                                <Typography
-                                  gutterBottom
-                                  variant="subtitle1"
-                                  component="div"
-                                >
-                                  {el.name}
-                                </Typography>
-                                <Typography variant="body2" gutterBottom>
-                                  {el.description}
-                                </Typography>
-                                <Typography
-                                  variant="body2"
-                                  color="text.secondary"
-                                >
-                                  {el.brand}
-                                </Typography>
+                      <Link to={`/${categorieSeccion}/${index}`}>
+                        <Paper
+                          className={classes.product}
+                          sx={{
+                            p: 2,
+                            margin: "10px",
+                            maxWidth: 1280,
+                            flexGrow: 1,
+                            backgroundColor: (theme) =>
+                              theme.palette.mode === "dark"
+                                ? "#1A2027"
+                                : "#fff",
+                          }}
+                          key={index}
+                        >
+                          <Grid container spacing={2}>
+                            <Grid item>
+                              <ButtonBase sx={{ width: 128, height: 128 }}>
+                                <Img alt={el.name} src={el.image} />
+                              </ButtonBase>
+                            </Grid>
+                            <Grid item xs={12} sm container>
+                              <Grid
+                                item
+                                xs
+                                container
+                                direction="column"
+                                spacing={2}
+                              >
+                                <Grid item xs>
+                                  <Typography
+                                    gutterBottom
+                                    variant="subtitle1"
+                                    component="div"
+                                  >
+                                    {el.name}
+                                  </Typography>
+                                  <Typography variant="body2" gutterBottom>
+                                    {el.description}
+                                  </Typography>
+                                  <Typography
+                                    variant="body2"
+                                    color="text.secondary"
+                                  >
+                                    {el.brand}
+                                  </Typography>
+                                </Grid>
+                                <Grid item>
+                                  <Typography
+                                    sx={{ cursor: "pointer" }}
+                                    variant="body2"
+                                  >
+                                    {"Favorites <3"}
+                                  </Typography>
+                                </Grid>
                               </Grid>
                               <Grid item>
-                                <Typography
-                                  sx={{ cursor: "pointer" }}
-                                  variant="body2"
-                                >
-                                  {"Favorites <3"}
+                                <Typography variant="subtitle1" component="div">
+                                  {"$" + el.price}
                                 </Typography>
                               </Grid>
                             </Grid>
-                            <Grid item>
-                              <Typography variant="subtitle1" component="div">
-                                {"$" + el.price}
-                              </Typography>
-                            </Grid>
                           </Grid>
-                        </Grid>
-                      </Paper>
+                        </Paper>
+                      </Link>
                     );
                   })}
                 </Grid>
