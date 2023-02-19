a4048cadf5069a56b178a8ca66e1dcbc3a68c515
becaale <becaale@hotmail.com>
2023-02-19 14:57:10 +0100

    mod(carousel): se modifico el nombre del componente - conflicto con mui

---
+++ src/components/Carousels/Carousels.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import Carousel from "react-material-ui-carousel";
+import Paper from "@mui/material/Paper";
+import { makeStyles } from "@mui/styles";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+
+const Carousels = () => {
+  const classes = useStyles();
+
+  const itemsByGroup = items.reduce((result, item, index) => {
+    const groupIndex = Math.floor(index / 6);
+    result[groupIndex] ??= [];
+    result[groupIndex].push(item);
+    return result;
+  }, []);
+
+  return (
+    <div style={{ padding: "30px" }}>
+      <Carousel
+        indicators={false}
+        autoPlay={false}
+        cycleNavigation={true}
+        animation="slide"
+        navButtonsAlwaysVisible={true}
+        slidesPerPage={1}
+        NextIcon={<ChevronRightIcon />}
+        PrevIcon={<ChevronLeftIcon />}
+      >
+        {itemsByGroup.map((group, index) => (
+          <Paper className={classes.paper} key={index}>
+            <div className={classes.items}>
+              {group.map((item, i) => (
+                <img key={i} src={item.image} alt="" className={classes.circle} />
+              ))}
+            </div>
+          </Paper>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
+
+const items = [
+  { image: "https://via.placeholder.com/60x60" },
+  { image: "https://via.placeholder.com/60x60" },
+  { image: "https://via.placeholder.com/60x60" },
+  { image: "https://via.placeholder.com/60x60" },
+  { image: "https://via.placeholder.com/60x60" },
+  { image: "https://via.placeholder.com/60x60" },
+  { image: "https://via.placeholder.com/60x60" },
+  { image: "https://via.placeholder.com/60x60" },
+  { image: "https://via.placeholder.com/60x60" },
+];
+const useStyles = makeStyles({
+  circle: {
+    width: "80px",
+    height: "80px",
+    borderRadius: "40px",
+    backgroundColor: "#000",
+  },
+  items: {
+    display: "flex",
+    justifyContent: "center",
+    gap: "10px",
+  },
+  /*   carouselButton: {
+    position: "absolute",
+    top: "calc(50% - 20px)",
+    width: "40px",
+    height: "40px",
+    borderRadius: "50%",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    color: "#fff",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    cursor: "pointer",
+    zIndex: 1,
+    transition: "opacity 0.2s",
+    "&:hover": {
+      opacity: 0.8,
+    },
+  },
+  carouselButtonPrev: {
+    left: "20px",
+  },
+  carouselButtonNext: {
+    right: "20px",
+  }, */
+});
+
+export default Carousels;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,34 +1,18 @@
 import Banner from "../Banner/Banner";
 import MosaicCategories from "../MosaicCategories/MosaicCategories";
 import Container from "@mui/material/Container";
-import Carousel from "../Carousel/Carousel";
+import Carousels from "../Carousels/Carousels";
 import { Box } from "@mui/system";
 import Stack from "@mui/material/Stack";
 
 const Home = () => {
-  /*const categoriesExample1 = [
-    {
-      title: "SmartPhones en descuento!!!",
-      img: "https://images.samsung.com/is/image/samsung/assets/co/smartphones/galaxy-z-flip3-5g/buy/ZFlip3_Carousel_FoldUnfoldCombo_Lavender_MO.jpg",
-    },
-    {
-      title: "televisions 30%",
-      img: "https://www.lg.com/co/images/televisores/md07504651/gallery/Des-01.jpg",
-    },
-    {
-      title: "refrigerators solo hoy",
-      img: "https://www.lg.com/co/images/neveras/md07538746/gallery/GB37WPT_1100-10.jpg",
-    },
-  ];*/
-
   return (
     <>
-      <Banner></Banner>
+      <Banner />
       <Container fixed>
         <Box sx={{ width: "100%" }}>
           <Stack direction="column" justifyContent="center" alignItems="center" spacing={6}>
-
-            <Carousel></Carousel>
+            <Carousels />
             <MosaicCategories />
           </Stack>
         </Box>
